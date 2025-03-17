5d1020ccda85c58da6d106444b906dbfd2a3840b
NikoleK <35893904+NikoleK@users.noreply.github.com>
2025-03-17 19:30:52 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Testers!");
         // new comment 123
         // where is my repo
         // 4564567
